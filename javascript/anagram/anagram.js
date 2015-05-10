2f212e65b4753588802bd62bd1aad9a6badf4acc
gyaresu <me@gareth.com.au>
2015-05-10 16:11:20 -0700

    anagram

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -3,12 +3,13 @@ var Anagram = function(word) {
 };
 
 Anagram.prototype.matches = function(arr){
-    var word = this.word;
+    var word = this.word.split().sort().join('');
+
     // re-read the problem. you need to return possible anagrams
 
-    // return arr.filter(function(each) {
-    //     return each === word;
-    // });
+    return arr.filter(function(each) {
+        return each.split().sort().join('') === word;
+    });
 };
 
 module.exports = Anagram;
