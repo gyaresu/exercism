eeabdcc10bdb1d7ca86e4c12b9d46fe0fb46c7cc
gyaresu <me@gareth.com.au>
2015-05-10 15:39:00 -0700

    anagram

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,5 +1,14 @@
-module.exports = function(word, list) {
-    return list.filter(function(each) {
-        return word === each;
-    });
+var Anagram = function(word) {
+    this.word = word;
 };
+
+Anagram.prototype.matches = function(arr){
+    var word = this.word;
+    // re-read the problem. you need to return possible anagrams
+
+    // return arr.filter(function(each) {
+    //     return each === word;
+    // });
+};
+
+module.exports = Anagram;
