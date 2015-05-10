82bc04256bd381c8b6f8bf0a2b1b611ec9697b80
gyaresu <me@gareth.com.au>
2015-05-10 14:50:58 -0700

    finished word-count

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,8 +1,9 @@
 module.exports = function(phrase) {
-    var words = phrase.split(' ');
+    var words = phrase.replace('\n', ' ').replace('\t', ' ').split(' ').
+        filter(function(each) { return each.length; });
     var count = {};
     words.map(function(word) {
-        !count[word] ? count[word] = 1: ++count[word];
+        !+count[word] ? count[word] = 1: ++count[word];
     });
     return count;
 };
