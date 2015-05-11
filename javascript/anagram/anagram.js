b0fab70dbfbb6df6045f53cbb6eaa700df96c4a9
gyaresu <me@gareth.com.au>
2015-05-10 21:04:51 -0700

    now args are strings not an array

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -7,10 +7,17 @@ var Anagram = function(word) {
 };
 
 Anagram.prototype.matches = function(arr){
+    if (process.argv.length > 1)
+        arr = Array.prototype.slice.call(arr);
+
+    var testWord = this.word;
     var sorted = this.word.toLowerCase().split('').sort().join('');
 
     return arr.filter(function(each) {
-        return each.toLowerCase().split('').sort().join('') === sorted;
+        if (each.toLowerCase() === testWord.toLowerCase())
+            return false;
+        else
+            return each.toLowerCase().split('').sort().join('') === sorted;
     });
 };
 
